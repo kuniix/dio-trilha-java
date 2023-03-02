2a767bff20082a887fddc65f461e3c11fedc2aab
Lucas Norato <lucasn.norato@gmail.com>
2023-03-02 18:55:52 -0300

    operadores logicos em java

---
+++ tipos-variaveis/src/dio/trilha/javabasico/operadoresLogicos.java
@@ -0,0 +1,45 @@
+package dio.trilha.javabasico;
+
+public class operadoresLogicos {
+    public static void main(String[] args) {
+        /*
+         * String nomeCompleto = "Linguagem " + "Java";
+         * System.out.println(nomeCompleto);
+         * 
+         * 
+         * int numero = -5;
+         * numero = -numero;
+         * 
+         * System.out.println(numero);
+         */
+
+        /*
+         * int numero = 5;
+         * // x repetição
+         * 
+         * numero = numero + 1;
+         * 
+         * System.out.println(numero);
+         */
+
+        /*
+         * int a, b;
+         * 
+         * a = 5;
+         * b = 5;
+         * String resultado = a != b ? "verdadeiro" : "falso";
+         * 
+         * System.out.println(resultado);
+         */
+
+        /*
+         * int numero1 = 2;
+         * int numero2 = 1;
+         * 
+         * Boolean simNao = numero1 > numero2;
+         * 
+         * System.out.println("NumeroUM é maior que numeroDOIS " + simNao);
+         */
+
+    }
+}
